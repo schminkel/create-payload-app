36f4a9103dca90955c29b710caa1490662cd8fb4
THORSTEN SCHMINKEL <thorsten.a.schminkel@capgemini.com>
2023-12-14 07:39:45 +0100

    .

--- payloadcms-website/next.config.js
+++ payloadcms-website/next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['localhost', process.env.NEXT_PUBLIC_SERVER_URL]
+    domains: ['localhost', process.env.NEXT_PUBLIC_IMAGES_DOMAIN, process.env.NEXT_PUBLIC_SERVER_URL]
       .filter(Boolean)
       .map(url => url.replace(/https?:\/\//, '')),
   },
